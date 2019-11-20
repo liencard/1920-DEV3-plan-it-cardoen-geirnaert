1a52655e954025e4561859e2371bfeda247da2de
Lieselot Geirnaert <lieselot.geirnaert@gmail.com>
2019-11-20 21:36:48 +0100

    add testdata to database and sql dump #31

--- assets/sportsplanner.sql
+++ assets/sportsplanner.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Nov 19, 2019 at 06:17 PM
+-- Generation Time: Nov 20, 2019 at 08:35 PM
 -- Server version: 5.7.26
 -- PHP Version: 7.3.8
 
@@ -19,6 +19,56 @@ SET time_zone = "+00:00";
 --
 -- Database: `sportsplanner`
 --
+CREATE DATABASE IF NOT EXISTS `sportsplanner` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `sportsplanner`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `activities`
+--
+
+CREATE TABLE `activities` (
+  `id` int(11) NOT NULL,
+  `sport_id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `starthour` time NOT NULL,
+  `endhour` time NOT NULL,
+  `location_id` int(11) NOT NULL,
+  `intensity` enum('light','medium','hard','') NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `activities`
+--
+
+INSERT INTO `activities` (`id`, `sport_id`, `date`, `starthour`, `endhour`, `location_id`, `intensity`, `timestamp`) VALUES
+(1, 1, '2019-11-22', '10:00:00', '11:00:00', 2, 'light', '2019-11-20 13:33:27'),
+(2, 5, '2019-11-30', '12:00:00', '12:40:00', 5, 'light', '2019-11-20 20:34:23');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `activities_have_focuses`
+--
+
+CREATE TABLE `activities_have_focuses` (
+  `id` int(11) NOT NULL,
+  `activity_id` int(11) NOT NULL,
+  `focus_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `activities_have_focuses`
+--
+
+INSERT INTO `activities_have_focuses` (`id`, `activity_id`, `focus_id`) VALUES
+(1, 1, 2),
+(2, 1, 3),
+(3, 2, 4),
+(4, 2, 1),
+(5, 2, 2);
 
 -- --------------------------------------------------------
 
@@ -31,6 +81,16 @@ CREATE TABLE `focuses` (
   `name` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `focuses`
+--
+
+INSERT INTO `focuses` (`id`, `name`) VALUES
+(1, 'flexibility'),
+(2, 'endurance'),
+(3, 'coordination'),
+(4, 'strength');
+
 -- --------------------------------------------------------
 
 --
@@ -39,8 +99,21 @@ CREATE TABLE `focuses` (
 
 CREATE TABLE `friends` (
   `id` int(11) NOT NULL,
-  `firstname` varchar(45) NOT NULL
+  `firstname` varchar(45) NOT NULL,
+  `activity_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `friends`
+--
+
+INSERT INTO `friends` (`id`, `firstname`, `activity_id`) VALUES
+(1, 'Lien', 0),
+(2, 'Sarah', 0),
+(3, 'Lieselot', 0),
+(4, 'Mathias', 0),
+(5, 'mama', 0),
+(6, 'papa', 0);
 
 -- --------------------------------------------------------
 
@@ -53,22 +126,16 @@ CREATE TABLE `locations` (
   `name` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `planned_activities`
+-- Dumping data for table `locations`
 --
 
-CREATE TABLE `planned_activities` (
-  `id` int(11) NOT NULL,
-  `date` date NOT NULL,
-  `starthour` time NOT NULL,
-  `endhour` time NOT NULL,
-  `location` int(11) NOT NULL,
-  `intensity` enum('light','medium','hard','') NOT NULL,
-  `friends` int(11) NOT NULL,
-  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+INSERT INTO `locations` (`id`, `name`) VALUES
+(1, 'sports hall'),
+(2, 'swimming pool'),
+(3, 'home'),
+(4, 'outside'),
+(5, 'gym');
 
 -- --------------------------------------------------------
 
@@ -79,8 +146,19 @@ CREATE TABLE `planned_activities` (
 CREATE TABLE `sports` (
   `id` int(11) NOT NULL,
   `name` varchar(45) NOT NULL,
-  `type` int(11) NOT NULL
+  `type_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `sports`
+--
+
+INSERT INTO `sports` (`id`, `name`, `type_id`) VALUES
+(1, 'football', 2),
+(2, 'rugby', 2),
+(3, 'tennis', 2),
+(4, 'basketball', 2),
+(5, 'ping pong', 2);
 
 -- --------------------------------------------------------
 
@@ -94,8 +172,28 @@ CREATE TABLE `types` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
+-- Dumping data for table `types`
+--
+
+INSERT INTO `types` (`id`, `name`) VALUES
+(1, 'extreme sport'),
+(2, 'ball sports');
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `activities`
+--
+ALTER TABLE `activities`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `activities_have_focuses`
+--
+ALTER TABLE `activities_have_focuses`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `focuses`
@@ -116,12 +214,6 @@ ALTER TABLE `locations`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `planned_activities`
---
-ALTER TABLE `planned_activities`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indexes for table `sports`
 --
 ALTER TABLE `sports`
@@ -138,40 +230,46 @@ ALTER TABLE `types`
 --
 
 --
+-- AUTO_INCREMENT for table `activities`
+--
+ALTER TABLE `activities`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `activities_have_focuses`
+--
+ALTER TABLE `activities_have_focuses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT for table `focuses`
 --
 ALTER TABLE `focuses`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `friends`
 --
 ALTER TABLE `friends`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `locations`
 --
 ALTER TABLE `locations`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `planned_activities`
---
-ALTER TABLE `planned_activities`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `sports`
 --
 ALTER TABLE `sports`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `types`
 --
 ALTER TABLE `types`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
