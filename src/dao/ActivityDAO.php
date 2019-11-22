157e3c6613b037d152af8b6e3a1825fbb8fc18a6
Lieselot Geirnaert <lieselot.geirnaert@gmail.com>
2019-11-22 22:06:20 +0100

    fix wrond database names to connect with heroku #44

--- src/dao/ActivityDAO.php
+++ src/dao/ActivityDAO.php
@@ -5,14 +5,14 @@ require_once( __DIR__ . '/DAO.php');
 class ActivityDAO extends DAO {
 
   public function selectAllLocations() {
-    $sql = "SELECT * FROM Locations";
+    $sql = "SELECT * FROM locations";
     $stmt = $this->pdo->prepare($sql);
     $stmt->execute();
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
   }
 
   public function selectAllTypes() {
-    $sql = "SELECT * FROM Types";
+    $sql = "SELECT * FROM types";
     $stmt = $this->pdo->prepare($sql);
     $stmt->execute();
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
