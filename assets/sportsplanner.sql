14494154d937dc9a905a079962c6ea83aedaa9ce
liencard <lien.cardoen@student.howest.be>
2019-11-24 19:40:18 +0100

    added more testdata & did some changes #48

--- assets/sportsplanner.sql
+++ assets/sportsplanner.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Nov 20, 2019 at 08:35 PM
--- Server version: 5.7.26
--- PHP Version: 7.3.8
+-- Generation Time: Nov 24, 2019 at 06:37 PM
+-- Server version: 5.7.23
+-- PHP Version: 7.2.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -19,8 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `sportsplanner`
 --
-CREATE DATABASE IF NOT EXISTS `sportsplanner` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `sportsplanner`;
 
 -- --------------------------------------------------------
 
@@ -45,7 +43,10 @@ CREATE TABLE `activities` (
 
 INSERT INTO `activities` (`id`, `sport_id`, `date`, `starthour`, `endhour`, `location_id`, `intensity`, `timestamp`) VALUES
 (1, 1, '2019-11-22', '10:00:00', '11:00:00', 2, 'light', '2019-11-20 13:33:27'),
-(2, 5, '2019-11-30', '12:00:00', '12:40:00', 5, 'light', '2019-11-20 20:34:23');
+(2, 5, '2019-11-30', '12:00:00', '12:40:00', 5, 'light', '2019-11-20 20:34:23'),
+(3, 2, '2019-11-26', '14:45:00', '16:00:00', 4, 'hard', '2019-11-24 16:23:52'),
+(4, 6, '2019-11-28', '19:30:00', '20:30:00', 5, 'medium', '2019-11-24 16:24:49'),
+(5, 4, '2019-11-30', '16:15:00', '18:30:00', 1, 'medium', '2019-11-24 16:26:23');
 
 -- --------------------------------------------------------
 
@@ -68,7 +69,12 @@ INSERT INTO `activities_have_focuses` (`id`, `activity_id`, `focus_id`) VALUES
 (2, 1, 3),
 (3, 2, 4),
 (4, 2, 1),
-(5, 2, 2);
+(5, 2, 2),
+(6, 3, 2),
+(7, 4, 1),
+(8, 4, 4),
+(9, 5, 3),
+(10, 5, 2);
 
 -- --------------------------------------------------------
 
@@ -108,12 +114,12 @@ CREATE TABLE `friends` (
 --
 
 INSERT INTO `friends` (`id`, `firstname`, `activity_id`) VALUES
-(1, 'Lien', 0),
-(2, 'Sarah', 0),
-(3, 'Lieselot', 0),
-(4, 'Mathias', 0),
-(5, 'mama', 0),
-(6, 'papa', 0);
+(1, 'Lien', 1),
+(2, 'Sarah', 3),
+(3, 'Lieselot', 4),
+(4, 'Mathias', 2),
+(5, 'mama', 5),
+(6, 'papa', 2);
 
 -- --------------------------------------------------------
 
@@ -123,19 +129,20 @@ INSERT INTO `friends` (`id`, `firstname`, `activity_id`) VALUES
 
 CREATE TABLE `locations` (
   `id` int(11) NOT NULL,
-  `name` varchar(45) NOT NULL
+  `location` varchar(45) NOT NULL,
+  `icon-location` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `locations`
 --
 
-INSERT INTO `locations` (`id`, `name`) VALUES
-(1, 'sports hall'),
-(2, 'swimming pool'),
-(3, 'home'),
-(4, 'outside'),
-(5, 'gym');
+INSERT INTO `locations` (`id`, `location`, `icon-location`) VALUES
+(1, 'sports hall', ''),
+(2, 'swimming pool', ''),
+(3, 'home', './assets/img/home.svg'),
+(4, 'outside', './assets/img/outside.svg'),
+(5, 'gym', './assets/img/gym.svg');
 
 -- --------------------------------------------------------
 
@@ -145,20 +152,22 @@ INSERT INTO `locations` (`id`, `name`) VALUES
 
 CREATE TABLE `sports` (
   `id` int(11) NOT NULL,
-  `name` varchar(45) NOT NULL,
-  `type_id` int(11) NOT NULL
+  `sport` varchar(45) NOT NULL,
+  `type_id` int(11) NOT NULL,
+  `icon-sport` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `sports`
 --
 
-INSERT INTO `sports` (`id`, `name`, `type_id`) VALUES
-(1, 'football', 2),
-(2, 'rugby', 2),
-(3, 'tennis', 2),
-(4, 'basketball', 2),
-(5, 'ping pong', 2);
+INSERT INTO `sports` (`id`, `sport`, `type_id`, `icon-sport`) VALUES
+(1, 'football', 2, './assets/img/football.svg'),
+(2, 'rugby', 2, './assets/img/rugby.svg'),
+(3, 'tennis', 2, './assets/img/tennis.svg'),
+(4, 'volleyball', 2, './assets/img/volleyball.svg'),
+(5, 'ping pong', 2, './assets/img/pingpong.svg'),
+(6, 'kickbox', 1, './assets/img/kickbox.svg');
 
 -- --------------------------------------------------------
 
@@ -233,13 +242,13 @@ ALTER TABLE `types`
 -- AUTO_INCREMENT for table `activities`
 --
 ALTER TABLE `activities`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `activities_have_focuses`
 --
 ALTER TABLE `activities_have_focuses`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `focuses`
@@ -263,7 +272,7 @@ ALTER TABLE `locations`
 -- AUTO_INCREMENT for table `sports`
 --
 ALTER TABLE `sports`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `types`
