adeaeb04090adaecf7ffb334f37ae81d320b5614
liencard <lien.cardoen@student.howest.be>
2019-11-30 15:18:48 +0100

    added empty page for when no activities #55

--- src/view/activities/index.php
+++ src/view/activities/index.php
@@ -1,17 +1,18 @@
 <section class="activities">
   <h2 class="activities__title">Planned Tasks</h2>
 
+  <?php if(!empty($activities)) { ?>
   <!-- <h3 class="activities__day">Today</h3> -->
     <div class="activites__day__wrapper">
-      <!-- Template -->
+
       <ul class="activities__list">
 
-        <?php
-          $previousDay = date('Y-m-d');
-        foreach ($activities as $activity) {
-          if($previousDay === $activity['date']) {
+        <?php foreach ($activities as $activity):
+        //   $previousDay = date('Y-m-d');
+        // foreach ($activities as $activity) {
+        //   if($previousDay === $activity['date']) {
         ?>
-        <h3 class="activities__day"><?php echo strftime("%a %d %B", strtotime($activity['date'])); ?></h3>
+        <h3 class="activities__day"><?php echo strftime("%a %d %B", strtotime($activity['date'])); ?></h3> <br>
         <li class="activity">
           <a class="activity__button" href="index.php?page=detail&id=<?php echo $activity['activity_id']; ?>">
             <img srcset="<?php echo $activity['sport_icon']; ?> 60w" sizes="40px" src="<?php echo $activity['sport_icon']; ?>" alt="Kickbox gloves icon">
@@ -21,14 +22,15 @@
             </span>
           </a>
         </li>
-        <?php  } } ?>
+        <?php endforeach;
+        // } }
+        ?>
 
       </ul>
     </div>
 
-  <h3 class="activities__day">Tomorrow</h3>
+  <!-- <h3 class="activities__day">Tomorrow</h3>
     <div class="activites__day__wrapper">
-    <!-- Template -->
     <ul class="activities__list">
       <li class="activity">
         <a class="activity__button" href="index.php?page=detail">
@@ -40,7 +42,17 @@
         </a>
       </li>
     </ul>
+  </div> -->
+
+  <?php } else {?>
+
+  <div class="noactivities">
+    <h3 class="noactivities__title">You have no planned activities</h3>
+    <img srcset="./assets/img/noactivity.svg 160w" sizes="160px" src="./assets/img/noactivity.svg" alt="Calendar icon">
   </div>
+
+  <?php }?>
+
 
   <a class="add__activity" href="index.php?page=add-activity">+</a>
 
